3a627eb540451715c5748d9047cbb445f8bf0e09
HermesonxN <hermesoninfo@gmail.com>
2023-11-08 11:31:06 -0300

    Buscador de CEP

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import './App.css';
+import { useState } from "react";
+import { FiSearch } from 'react-icons/fi';
+import api from "./Api/ViaCep";
+function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState('');
+
+  async function handleSearch(){ 
+    if(input === ''){
+      alert("Preencha com um cep válido");
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput('');
+    }catch{
+      alert("Erro ao buscar CEP");
+      setInput('');
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">Buscador</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu cep"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#fff"/>
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
+    </div>
+  )
+};
+
+export default App;
